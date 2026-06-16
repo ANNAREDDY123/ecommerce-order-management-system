3521fe0a77786957eb8065682300b86ae77a9dba
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-16 09:37:25 +0530

    Create customer.py

---
+++ models/customer.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+
+    email = Column(
+        String,
+        unique=True
+    )
