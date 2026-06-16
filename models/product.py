4229d7a8b64f9d311c87df821c2ada1fb703d1fe
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-16 09:37:55 +0530

    Create product.py

---
+++ models/product.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Float,
+    Boolean
+)
+
+from database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    name = Column(String)
+
+    category = Column(String)
+
+    price = Column(Float)
+
+    stock = Column(Integer)
+
+    is_active = Column(
+        Boolean,
+        default=True
+    )
