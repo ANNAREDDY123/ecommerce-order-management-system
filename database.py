39a78f96882106bc7b742f73909a0c715db36207
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-16 16:43:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,14 +6,12 @@ DATABASE_URL = "sqlite:///./ecommerce.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+    connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
-)
+    bind=engine)
 
 Base = declarative_base()
 
